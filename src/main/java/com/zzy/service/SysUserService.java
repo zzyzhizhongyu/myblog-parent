f3f2c341642d5b5b14d936675dcb78ef84829381
zzyzhizhongyu <2693991535@qq.com>
2021-09-25 17:02:51 +0800

    Revert "注册"
    This reverts commit 976e5781

--- src/main/java/com/zzy/service/SysUserService.java
+++ src/main/java/com/zzy/service/SysUserService.java
@@ -1,17 +1,7 @@
 package com.zzy.service;
 
 import com.zzy.dao.pojo.SysUser;
-import com.zzy.vo.Result;
 
 public interface SysUserService {
-
-    SysUser findUser(String account, String password);
-
     SysUser findUserById(Long id);
-
-    Result findUserByToken(String token);
-
-    SysUser findUserByAccount(String account);
-
-    void save(SysUser sysUser);
 }
